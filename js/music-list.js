1cb9f25dc584eaeba47b56f268c28f6c343d3ad2
Anh Pham <anhpnd12@gmail.com>
2023-09-21 14:36:33 -0400

    playlist update 1

---
+++ js/music-list.js
@@ -0,0 +1,14 @@
+let allMusic = [
+  {
+    name: "The Only Exception",
+    artist: "Paramore",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Easy",
+    artist: "Mac Ayres",
+    img: "music-2",
+    src: "music-2"
+  },
+];
